8cd06046fc2bd4e0d644beb1c52f3e3b17307f12
bahuan-coding <andrebahuann@gmail.com>
2025-12-23 12:37:24 -0300

    Reset repository - fresh start

--- engine/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Utility subpackage for logging and terminal table helpers."""
-
-from .position_sizing import round_b3_board_lot, calculate_tranche_quantity
-
-__all__ = ['round_b3_board_lot', 'calculate_tranche_quantity']
-
-
